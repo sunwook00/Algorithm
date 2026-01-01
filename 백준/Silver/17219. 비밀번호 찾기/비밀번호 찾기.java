b057b673ffde14a864a9ff0833047ce901a63973
sunwook00 <kckk116560@gmail.com>
2026-01-01 23:01:04 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 536 ms, Memory: 55628 KB -BaekjoonHub

---
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        
+        Map<String, String> map = new HashMap<>();
+        
+        for(int i =0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            String site = st.nextToken();
+            String password = st.nextToken();
+            
+            map.put(site, password);
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < M; i++) {
+            String okSite = br.readLine();
+            String okSitePassword = map.get(okSite);
+            
+            sb.append(okSitePassword).append('\n');
+        }
+        
+        System.out.println(sb);
+    }
+}

55374ae3af67ce3188cdc375c21a2dc49fa8eaf3
sunwook00 <kckk116560@gmail.com>
2026-01-03 20:18:15 +0900

    [Silver IV] Title: 배부른 마라토너, Time: 388 ms, Memory: 33232 KB -BaekjoonHub

---
+++ 백준/Silver/10546. 배부른 마라토너/배부른 마라토너.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        Map<String, Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < N; i++) {
+            String name = br.readLine();
+            map.put(name, map.getOrDefault(name, 0) + 1);
+        }
+        
+        for(int i = 0; i < N - 1; i++) {
+            String name = br.readLine();
+            map.put(name, map.get(name) -1);
+        }
+        
+        for(String key : map.keySet()) {
+            if(map.get(key) != 0) {
+                System.out.println(key);
+            }
+        }
+    }
+}
